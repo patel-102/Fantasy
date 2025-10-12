f0c4a7c358a5b335604e23bab123e2e50d303f86
patel-102 <pv6683434@gmail.com>
2025-10-12 20:13:37 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -4,12 +4,12 @@ import { getFirestore } from "https://www.gstatic.com/firebasejs/12.3.0/firebase
 import { getStorage } from "https://www.gstatic.com/firebasejs/12.3.0/firebase-storage.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCS4-Oc9jeWaQGQMRYoIL_2y2O5nrKZ6gM",
-  authDomain: "fantasy-41fc9.firebaseapp.com",
-  projectId: "fantasy-41fc9",
-  storageBucket: "fantasy-41fc9.appspot.com",
-  messagingSenderId: "431557761003",
-  appId: "1:431557761003:web:375881a847d1d8b9506bae"
+  apiKey: "AIzaSyAQLK28ZvU2L09u1OIHlBlrGZZ7TPXEF8g",
+  authDomain: "futurex-9a2e9.firebaseapp.com",
+  projectId: "futurex-9a2e9",
+  storageBucket: "futurex-9a2e9.firebasestorage.app",
+  messagingSenderId: "82126141815",
+  appId: "1:82126141815:web:20fcd85f32feac7553c972"
 };
 
 const app = initializeApp(firebaseConfig);
