2cedf70cba2fa16b0cf4ebcfaac125c9d1599581
patel-102 <pv6683434@gmail.com>
2025-10-17 20:57:26 +0530

    Update private.js

--- private.js
+++ private.js
@@ -7,13 +7,12 @@ import { getStorage } from "https://www.gstatic.com/firebasejs/12.3.0/firebase-s
 
 // Your Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCS4-Oc9jeWaQGQMRYoIL_2y2O5nrKZ6gM",
-  authDomain: "fantasy-41fc9.firebaseapp.com",
-  databaseURL: "https://fantasy-41fc9-default-rtdb.firebaseio.com",
-  projectId: "fantasy-41fc9",
-  storageBucket: "fantasy-41fc9.firebasestorage.app",
-  messagingSenderId: "431557761003",
-  appId: "1:431557761003:web:375881a847d1d8b9506bae"
+  apiKey: "AIzaSyAcM6iATwM70jFkYXRb6bT7KXLGZrgai6E",
+  authDomain: "futurex2-a8030.firebaseapp.com",
+  projectId: "futurex2-a8030",
+  storageBucket: "futurex2-a8030.firebasestorage.app",
+  messagingSenderId: "772713861227",
+  appId: "1:772713861227:web:ab4cde62c1cc33121a212e"
 };
 
 
