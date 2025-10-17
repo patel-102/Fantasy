66d51c6f0a2ba160fc4cd7caed46ff42c78cd9e4
patel-102 <pv6683434@gmail.com>
2025-10-17 22:20:26 +0530

    Add files via upload

--- private.js
+++ private.js
@@ -1,24 +1,27 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/12.4.0/firebase-app.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/12.4.0/firebase-auth.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/12.4.0/firebase-firestore.js";
-import { getStorage } from "https://www.gstatic.com/firebasejs/12.4.0/firebase-storage.js";
+// firebase.js
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.3.0/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/12.3.0/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/12.3.0/firebase-firestore.js";
+import { getStorage } from "https://www.gstatic.com/firebasejs/12.3.0/firebase-storage.js";
 
 // Your Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCo6Sz7bIhdYPFKxpy---q8_s0m__GN3_E",
-  authDomain: "system-2-2c3c4.firebaseapp.com",
-  databaseURL: "https://system-2-2c3c4-default-rtdb.firebaseio.com",
-  projectId: "system-2-2c3c4",
-  storageBucket: "system-2-2c3c4.appspot.com",
-  messagingSenderId: "308422442069",
-  appId: "1:308422442069:web:eed6c628239ffed3dd68dc",
-  measurementId: "G-6EP3E503HX"
+  apiKey: "AIzaSyC1270K5HA3GnbizEfmQ4e65VbMJSlH1gY",
+  authDomain: "private-5b36a.firebaseapp.com",
+  databaseURL: "https://private-5b36a-default-rtdb.firebaseio.com",
+  projectId: "private-5b36a",
+  storageBucket: "private-5b36a.firebasestorage.app",
+  messagingSenderId: "929389405583",
+  appId: "1:929389405583:web:277f2ac3d5bc7ee543d7fa"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
-// Export initialized Firebase services
-export const auth = getAuth(app);
-export const db = getFirestore(app);
-export const storage = getStorage(app);
+// Initialize Firebase services
+const auth = getAuth(app);
+const db = getFirestore(app);
+const storage = getStorage(app);
+
+// Export for other scripts
+export { app, auth, db, storage };
