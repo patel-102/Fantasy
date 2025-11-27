25f305af650f1d399a8b8e5570b3c36691da2d5a
patel-102 <pv6683434@gmail.com>
2025-11-27 16:00:40 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -2,17 +2,19 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/12.6.0/firebas
 import { getAuth } from "https://www.gstatic.com/firebasejs/12.6.0/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/12.6.0/firebase-firestore.js";
 import { getStorage } from "https://www.gstatic.com/firebasejs/12.6.0/firebase-storage.js";
+import { getDatabase } from "https://www.gstatic.com/firebasejs/12.6.0/firebase-database.js";
 
 // Your Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAcM6iATwM70jFkYXRb6bT7KXLGZrgai6E",
-  authDomain: "futurex2-a8030.firebaseapp.com",
-  databaseURL: "https://futurex2-a8030-default-rtdb.firebaseio.com",
-  projectId: "futurex2-a8030",
-  storageBucket: "futurex2-a8030.firebasestorage.app",
-  messagingSenderId: "772713861227",
-  appId: "1:772713861227:web:ab4cde62c1cc33121a212e"
+  apiKey: "AIzaSyC-_Fz9GQ5zqj5p_mLLIyQpsq4gyhG_FqI",
+  authDomain: "futurex-1e0ae.firebaseapp.com",
+  databaseURL: "https://futurex-1e0ae-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "futurex-1e0ae",
+  storageBucket: "futurex-1e0ae.firebasestorage.app",
+  messagingSenderId: "872630439870",
+  appId: "1:872630439870:web:3c7e98b9157ffa684733ef"
 };
+
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
@@ -20,3 +22,4 @@ const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
 export const storage = getStorage(app);
+export const rtdb = getDatabase(app);   // <<--- REALTIME DATABASE
