7545465ae0a8c9cde53ed62a5a1059f5899e0f4c
patel-102 <pv6683434@gmail.com>
2025-11-27 14:06:35 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,19 +1,18 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/12.5.0/firebase-app.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/12.5.0/firebase-auth.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/12.5.0/firebase-firestore.js";
-import { getStorage } from "https://www.gstatic.com/firebasejs/12.5.0/firebase-storage.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.6.0/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/12.6.0/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/12.6.0/firebase-firestore.js";
+import { getStorage } from "https://www.gstatic.com/firebasejs/12.6.0/firebase-storage.js";
 
 // Your Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAQLK28ZvU2L09u1OIHlBlrGZZ7TPXEF8g",
-  authDomain: "futurex-9a2e9.firebaseapp.com",
-  databaseURL: "https://futurex-9a2e9-default-rtdb.firebaseio.com",
-  projectId: "futurex-9a2e9",
-  storageBucket: "futurex-9a2e9.firebasestorage.app",
-  messagingSenderId: "82126141815",
-  appId: "1:82126141815:web:20fcd85f32feac7553c972"
+  apiKey: "AIzaSyAcM6iATwM70jFkYXRb6bT7KXLGZrgai6E",
+  authDomain: "futurex2-a8030.firebaseapp.com",
+  databaseURL: "https://futurex2-a8030-default-rtdb.firebaseio.com",
+  projectId: "futurex2-a8030",
+  storageBucket: "futurex2-a8030.firebasestorage.app",
+  messagingSenderId: "772713861227",
+  appId: "1:772713861227:web:ab4cde62c1cc33121a212e"
 };
-
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
