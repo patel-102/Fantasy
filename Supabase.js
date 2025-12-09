278fb144aa5e09ce1fb1e094aafbef42b0f5c57f
patel-102 <pv6683434@gmail.com>
2025-12-09 11:14:56 +0530

    Update Supabase.js

--- Supabase.js
+++ Supabase.js
@@ -7,7 +7,7 @@ import { createClient } from "https://esm.sh/@supabase/supabase-js";
 // 🔐 Supabase Config
 // =========================================================
 const SUPABASE_URL = "https://ghxjxziiuxvutevkelet.supabase.co";
-const SUPABASE_ANON_KEY =eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdoeGp4emlpdXh2dXRldmtlbGV0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjUyMzY0MjAsImV4cCI6MjA4MDgxMjQyMH0.93O4ROK8G84wYuqfGUrmCxvcA3LvcIRuDoL4SVJv7eE";
+const SUPABASE_ANON_KEY ="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdoeGp4emlpdXh2dXRldmtlbGV0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjUyMzY0MjAsImV4cCI6MjA4MDgxMjQyMH0.93O4ROK8G84wYuqfGUrmCxvcA3LvcIRuDoL4SVJv7eE";
 // =========================================================
 // 🚀 Create Supabase Client
 // =========================================================
