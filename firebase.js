3a20b125e80013f3ee642f14af8d87e44b184c07
patel-102 <pv6683434@gmail.com>
2025-12-13 14:59:11 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -3,14 +3,12 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/12.6.0/firebas
 import { getAuth } from "https://www.gstatic.com/firebasejs/12.6.0/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/12.6.0/firebase-firestore.js";
 import { getDatabase } from "https://www.gstatic.com/firebasejs/12.6.0/firebase-database.js";
-import { getStorage } from "https://www.gstatic.com/firebasejs/12.6.0/firebase-storage.js";
 
 // Your actual Firebase Config
 const firebaseConfig = {
   apiKey: "AIzaSyA3A3UMK41OJa8nB_ZfCDzuuOBnV8GEw44",
   authDomain: "future-7cd4d.firebaseapp.com",
   projectId: "future-7cd4d",
-  storageBucket: "future-7cd4d.firebasestorage.app",
   messagingSenderId: "134358915216",
   appId: "1:134358915216:web:72c425a3fac47d5e60a015"
 };
@@ -22,7 +20,6 @@ const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const db = getFirestore(app);
 const rtdb = getDatabase(app);
-const storage = getStorage(app);
 
 // Export services
-export { auth, db, rtdb, storage };
+export { auth, db, rtdb };
