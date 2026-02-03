98d46d4cd8b95c8c43d6387ab0e3ee97855d550b
patel-102 <pv6683434@gmail.com>
2026-02-03 17:08:18 +0530

    Fix formatting of firebaseConfig object

--- firebase.js
+++ firebase.js
@@ -5,13 +5,13 @@ import { getFirestore } from "https://www.gstatic.com/firebasejs/12.8.0/firebase
 import { getDatabase } from "https://www.gstatic.com/firebasejs/12.8.0/firebase-database.js";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyAgauezxBjk7w6hf_gNvv0O2xGBB7yNdTk",
-    authDomain: "myshop-4a2f3.firebaseapp.com",
-    projectId: "myshop-4a2f3",
-    storageBucket: "myshop-4a2f3.firebasestorage.app",
-    messagingSenderId: "732248120499",
-    appId: "1:732248120499:web:73809ec93ba0f530b1a4c6"
-  };
+  apiKey: "AIzaSyAgauezxBjk7w6hf_gNvv0O2xGBB7yNdTk",
+  authDomain: "myshop-4a2f3.firebaseapp.com",
+  projectId: "myshop-4a2f3",
+  storageBucket: "myshop-4a2f3.firebasestorage.app",
+  messagingSenderId: "732248120499",
+  appId: "1:732248120499:web:73809ec93ba0f530b1a4c6"
+};
 
 
 // Initialize Firebase
